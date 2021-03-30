e9ac3e4cc194501cde10b8bb354eb72641ff6f50
brochj <brochj@gmail.com>
2021-03-29 21:59:25 -0300

    Comment Tag Manager code

--- pages/_document.js
+++ pages/_document.js
@@ -8,8 +8,8 @@ export default class MyDocument extends Document {
         <Head />
         <body className="dark:bg-gray-900">
           {/* <!-- Google Tag Manager (noscript) --> */}
-          <noscript><iframe src={`https://www.googletagmanager.com/ns.html?id=${GA_TRACKING_ID}`}
-          className="h-0 w-0 hidden invisible"></iframe></noscript>
+          {/* <noscript><iframe src={`https://www.googletagmanager.com/ns.html?id=${GA_TRACKING_ID}`}
+          className="h-0 w-0 hidden invisible"></iframe></noscript> */}
           {/* <!-- End Google Tag Manager (noscript) --> */}
           <Main />
           <NextScript />
