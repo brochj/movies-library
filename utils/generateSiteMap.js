8aea3ecc0cb2fbdf56824926cec2223ff2a81d9c
brochj <brochj@gmail.com>
2021-04-12 11:32:45 -0300

    Fix Generate sitemap

--- utils/generateSiteMap.js
+++ utils/generateSiteMap.js
@@ -7,36 +7,52 @@ async function generateSiteMap() {
   const pages = await globby([
     'pages/**/*.js',
     '!pages/_*.js',
-    '!pages/**/[slug].js',
-    '!pages/**/[tag].js',
     '!pages/**/[page].js',
     '!pages/**/[movie].js',
     '!pages/**/[genre].js',
+    '!pages/**/[imdb].js',
     '!pages/[movie].js',
     '!pages/api',
+  ])
+
+  const movies = await globby([
+    '!pages/**/*.js',
     '_movies/*.md',
   ])
 
   const sitemap = 
 `<?xml version="1.0" encoding="UTF-8"?>
 <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
-    ${pages
+  ${pages
+    .map(page => {
+      const path = page
+        .replace('pages/', '')
+        .replace('.js', '')
+        .replace('/index', '')
+        .replace('index', '')
+      return `
+  <url>
+    <loc>${`https://brokfilmes.vercel.app/${path}`}</loc>
+    <lastmod>${time}</lastmod>
+  </url>
+          `
+    })
+    .join('')}
+    ${movies
       .map(page => {
         const path = page
           .replace('_movies/', '')
-          .replace('/filmes/index', 'filmes')
-          .replace('pages', '')
-          .replace('.js', '')
           .replace('.md', '')
-        const route = path === '/index' ? '' : path
         return `
   <url>
-    <loc>${`https://download-filmes.vercel.app/${route}`}</loc>
+    <loc>${`https://brokfilmes.vercel.app/filme/${path}`}</loc>
     <lastmod>${time}</lastmod>
   </url>
             `
       })
       .join('')}
+
+    
 </urlset>
 `
 
