4e16466b6e756a9b02509c7e2ebdb4162c62f022
brochj <brochj@gmail.com>
2021-05-12 19:04:47 -0300

    Remove next/image component due reate limits

--- components/movie/cover-card.js
+++ components/movie/cover-card.js
@@ -1,18 +1,13 @@
-import cn from 'classnames'
 import Link from 'next/link'
-import Image from 'next/image'
 
 export default function CoverCard({ title, src, slug, height, width }) {
   const image = (
-    <Image
+    <img
       src={src}
       alt={`Imagem da capa do filme ${title}`}
-      className={cn('shadow-sm', {
-        'hover:opacity-60 transition-opacity duration-200': slug,
-      }, 'rounded-md')}
-      layout="responsive"
-      width={width}
-      height={height}
+      className="w-full"
+      width={width.toString()}
+      height={height.toString()}
     />
   )
   return (
--- components/movie/movie-cover.js
+++ components/movie/movie-cover.js
@@ -1,9 +1,8 @@
-import Image from 'next/image'
 
 export default function MovieCover({ title, src, height, width }) {
   return (
     <div className="sm:mx-0">
-      <Image
+      <img
         src={src}
         alt={`Imagem da capa do filme ${title}`}
         className='shadow-sm hover:opacity-80 transition-opacity duration-200 rounded-t-md'
